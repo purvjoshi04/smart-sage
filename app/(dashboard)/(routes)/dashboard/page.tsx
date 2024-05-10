0afc9631dc30dda3eed29a461485f9eaf9d92871
Purv Joshi <purvadjoshi@gmail.com>
2024-05-10 13:04:24 +0530

    finished dashboard user interface

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,82 @@
-// import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { MessageTwoTone, ArrowForwardTwoTone, BrokenImageTwoTone, VideoCameraBackTwoTone, MusicNoteTwoTone, CodeTwoTone } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageTwoTone,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Image Generation",
+    icon: BrokenImageTwoTone,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoCameraBackTwoTone,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/conversation"
+  },
+  {
+    label: "Music Generation",
+    icon: MusicNoteTwoTone,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Code Generation",
+    icon: CodeTwoTone,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/conversation"
+  },
+]
 
 
 const DashboardPage = () => {
+
+  const router = useRouter();
+
   return (
     <div>
-      <p>Dashboard page</p>
-      
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex justify-between items-center hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-4 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-6 h-6", tool.color)} />
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowForwardTwoTone className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 // import { Code, ImageIcon, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import { DashboardTwoTone, MessageTwoTone, BrokenImageTwoTone, VideoCameraBackTwoTone, MusicNoteTwoTone, CodeTwoTone, SettingsTwoTone } from "@mui/icons-material";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat(
     {
@@ -58,6 +59,9 @@ const routes = [
 ]
 
 const Sidebar = () => {
+
+    const pathName = usePathname()
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -78,7 +82,9 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathName === route.href ? "text-white bg-white/10" : "text-zinc-400"
+                            )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-7 w-7 mr-3", route.color)} />
