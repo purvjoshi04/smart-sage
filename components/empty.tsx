f3fd0184c6457750e952ddc6dcb84d62c6355b7b
Purv Joshi <purvadjoshi@gmail.com>
2024-05-15 15:24:18 +0530

    fixed empty conversation page image issue

--- components/empty.tsx
+++ components/empty.tsx
@@ -10,8 +10,8 @@ export const Empty = ({
 }: EmptyProps) => {
     return (
         <div className="h-full p-20 flex flex-col items-center justify-center">
-            <div className="relative h-73 w-73">
-                <Image alt="Empty" src="/empty.png" height={600} width={600}/>
+            <div className="relative h-72 w-72">
+                <Image alt="Empty" src="/empty.png" quality={75} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
             </div>
             <p className="text-muted-foreground text-sm text-center pt-7 pl-10">
                 {label}
